a05c7b37c315464ba6b615cf20e75e27ec3c8eb5
kamataryo <kamataryo@users.noreply.github.com>
2020-07-25 10:59:59 +0900

    Return 204

---
+++ functions/204.js
@@ -0,0 +1,9 @@
+exports.handler = function(event, context, callback) {
+  callback(null, {
+    statusCode: 204,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+    body: 'no content.'
+  })
+}
